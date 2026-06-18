60805fe21a13212f5adbc45742370a8a22ade770
drblack0 <kartik03012@gmail.com>
2026-06-18 22:07:24 +0530

    completed the vector library

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,4 +1,7 @@
-use std::ops::{AddAssign, DivAssign, Index, IndexMut, MulAssign, Neg};
+use std::{
+    fmt,
+    ops::{Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Neg, Sub, SubAssign},
+};
 
 #[derive(Debug)]
 pub struct Vec3 {
@@ -12,14 +15,24 @@ impl Vec3 {
         }
     }
 
-    pub fn length_squared(self) -> f64 {
+    pub fn length_squared(&self) -> f64 {
         self.elements[0] * self.elements[0]
             + self.elements[1] * self.elements[1]
             + self.elements[2] * self.elements[2]
     }
 
-    pub fn length(self) -> f64 {
+    pub fn length(&self) -> f64 {
         self.length_squared().sqrt()
+    }
+
+    pub fn x(&self) -> f64 {
+        self.elements[0]
+    }
+    pub fn y(&self) -> f64 {
+        self.elements[1]
+    }
+    pub fn z(&self) -> f64 {
+        self.elements[2]
     }
 }
 
@@ -46,11 +59,51 @@ impl IndexMut<usize> for Vec3 {
     }
 }
 
+impl Add for Vec3 {
+    type Output = Vec3;
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec3::new(
+            self.elements[0] + rhs.elements[0],
+            self.elements[1] + rhs.elements[1],
+            self.elements[2] + rhs.elements[2],
+        )
+    }
+}
+
 impl AddAssign for Vec3 {
     fn add_assign(&mut self, rhs: Self) {
         self.elements[0] += rhs.elements[0];
         self.elements[1] += rhs.elements[1];
         self.elements[2] += rhs.elements[2];
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Vec3::new(
+            self.elements[0] * rhs.elements[0],
+            self.elements[1] * rhs.elements[1],
+            self.elements[2] * rhs.elements[2],
+        )
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs: f64) -> Self::Output {
+        Vec3::new(
+            self.elements[0] * rhs,
+            self.elements[1] * rhs,
+            self.elements[2] * rhs,
+        )
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        rhs * self
     }
 }
 
@@ -62,10 +115,104 @@ impl MulAssign for Vec3 {
     }
 }
 
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.elements[0] *= rhs;
+        self.elements[1] *= rhs;
+        self.elements[2] *= rhs;
+    }
+}
+
+impl Div for Vec3 {
+    type Output = Vec3;
+    fn div(self, rhs: Self) -> Self::Output {
+        Vec3::new(
+            self.elements[0] / rhs.elements[0],
+            self.elements[1] / rhs.elements[1],
+            self.elements[2] / rhs.elements[2],
+        )
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+    fn div(self, rhs: f64) -> Self::Output {
+        self * (1.0 / rhs)
+    }
+}
+
+impl Div<f64> for &Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        // self is &Vec3, we have to access elements via &self.elements
+        Vec3::new(
+            self.elements[0] / rhs,
+            self.elements[1] / rhs,
+            self.elements[2] / rhs,
+        )
+    }
+}
+
 impl DivAssign for Vec3 {
     fn div_assign(&mut self, rhs: Self) {
         self.elements[0] /= rhs.elements[0];
         self.elements[1] /= rhs.elements[1];
         self.elements[2] /= rhs.elements[2];
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        // The C++ code optimizes this: *this *= 1/t
+        let inv = 1.0 / rhs;
+        self.elements[0] *= inv;
+        self.elements[1] *= inv;
+        self.elements[2] *= inv;
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vec3::new(
+            self.elements[0] - rhs.elements[0],
+            self.elements[1] - rhs.elements[1],
+            self.elements[2] - rhs.elements[2],
+        )
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.elements[0] -= rhs.elements[0];
+        self.elements[1] -= rhs.elements[1];
+        self.elements[2] -= rhs.elements[2];
+    }
+}
+
+pub fn dot(u: &Vec3, v: &Vec3) -> f64 {
+    u.elements[0] * v.elements[0] + u.elements[1] * v.elements[1] + u.elements[2] * v.elements[2]
+}
+
+pub fn cross(u: &Vec3, v: &Vec3) -> Vec3 {
+    Vec3::new(
+        u.elements[1] * v.elements[2] - u.elements[2] * v.elements[1],
+        u.elements[2] * v.elements[0] - u.elements[0] * v.elements[2],
+        u.elements[0] * v.elements[1] - u.elements[1] * v.elements[0],
+    )
+}
+
+pub fn unit_vector(v: &Vec3) -> Vec3 {
+    v / v.length()
+}
+
+impl fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} {} {}",
+            self.elements[0], self.elements[1], self.elements[2]
+        )
     }
 }
